ed520c23392961d7e9d91e7297abf716834b2eab
sharada79 <sharadaghorpade79@gmail.com>
2025-02-03 13:44:16 +0000

    operations

---
+++ basics/conversionoperation.js
@@ -0,0 +1,20 @@
+let score = "33abc"
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+// NaN - not an number
+// boolean T-1 , F/empty-0
+// *************************Operations************************************
+// console.log(2**3); power
+// console.log(2%3); Reminder
+
+// console.log("1" + 2 + 2); string op is 1st then all of them consider string op
+
+//  ************************Comparisions**********************************
+// console.log(null >= 0);
+// equality check == and 
+// Comparisions > >= < <= works diff     ...comparisions treat null as 0
+// strict check ===  ...checks both value and datatype
