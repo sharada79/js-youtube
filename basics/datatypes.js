99a4345e75b492862343b8c4523b7f9976006c3d
sharada79 <sharadaghorpade79@gmail.com>
2025-02-03 12:19:04 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+// alert()
+
+console.log(3+3);
+
+console.log("sharada");
+
+let name = "sharada"
+let age = 20
+let isLoggedIn = false
+// PRIMITIVE DATATYPES
+// number, bigint
+// string =""
+// boolean
+// null - standalone value 
+// undefined
+// symbol - (for uniqueness)
+
+// object
+
+console.log(typeof"sharada");
+console.log(typeof null);
