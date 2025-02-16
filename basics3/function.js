ba4d093baf0a43767f97174eb9b64f4ca283701a
sharada79 <sharadaghorpade79@gmail.com>
2025-02-16 18:53:57 +0000

    function

---
+++ basics3/function.js
@@ -0,0 +1,29 @@
+// functKEYWORD functNAME functSYNTAX
+function sayMyName(){
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("R");
+    console.log("A");
+    console.log("D");
+    console.log("A");
+}
+// call funct
+//sayMyName ...//its a referance
+//sayMyName() //...execution
+                       //parameters
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+// }
+function addTwoNumbers(number1, number2){
+    let result= number1 + number2;
+    //OR
+    return number1 + number2
+    return result
+    console.log();  //after result doesn't print anything
+}
+                          //arguments
+const result = addTwoNumbers(3,5)
+console.log("Result", result);
+
+             
