78bad3081809966b23fb7eea73149c04ed9eb5de
sharada79 <sharadaghorpade79@gmail.com>
2025-04-30 21:12:22 +0000

    projects

---
+++ Project1/one.js
@@ -0,0 +1,19 @@
+buttons.forEach (function (button) {
+    console.log(button);
+    button.addEventListener('click', function (e) {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id == 'grey'){
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id == 'white'){
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id == 'pink'){
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id == 'yellow'){
+            body.style.backgroundColor = e.target.id
+        }
+    });
+});
